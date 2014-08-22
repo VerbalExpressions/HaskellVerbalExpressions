1265663b810d91adb132d741a325770b28abec3a
Jett <whackashoe@gmail.com>
2014-08-22 04:03:09 -0500

    update formatting

--- examples.hs
+++ examples.hs
@@ -3,30 +3,30 @@ import Control.Arrow
 
 main :: IO()
 main = do
-	--create an example of how to test for correctly formed URLs
-	let expr = searchGlobal >>>
-			startOfLine >>>
-			find "http" >>>
-			possibly "s" >>>
-			find "://" >>>
-			possibly "www" >>>
-			anythingBut " " >>>
-			endOfLine
-			$ verEx
+    --create an example of how to test for correctly formed URLs
+    let expr = searchGlobal >>>
+            startOfLine     >>>
+            find "http"     >>>
+            possibly "s"    >>>
+            find "://"      >>>
+            possibly "www"  >>>
+            anythingBut " " >>>
+            endOfLine
+            $ verEx
 
-	-- Use VerEx's test function to find if it matches
-	print $ test "http://www.google.com" expr
+    -- Use VerEx's test function to find if it matches
+    print $ test "http://www.google.com" expr
 
-	--Ouputs the actual expression used: ^(?:http)(?:s)?(?:://)(?:www.)?(?:[^ ]*)$
-	print $ expr
+    --Ouputs the actual expression used: ^(?:http)(?:s)?(?:://)(?:www.)?(?:[^ ]*)$
+    print $ expr
 
-	-- Create a test string
-	let replaceMe = "Replace bird with a duck"
-	-- Create an expression that seeks for word "bird"
-	let expr2 = find "bird" $ verEx;
-	
-	-- Execute the expression
-	print $ replace replaceMe "duck" expr2
+    -- Create a test string
+    let replaceMe = "Replace bird with a duck"
+    -- Create an expression that seeks for word "bird"
+    let expr2 = find "bird" $ verEx;
+    
+    -- Execute the expression
+    print $ replace replaceMe "duck" expr2
 
-	-- Shorthand string replace
-	print $ replace "We have a red house" "blue" . find "red" $ verEx
+    -- Shorthand string replace
+    print $ replace "We have a red house" "blue" . find "red" $ verEx
